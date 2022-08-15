b3cdc1c2c553c7fae19663625e6586c4911e93af
amanmehra262001 <amanmehra262001@gmail.com>
2022-08-15 09:22:17 +0530

    Neo to Matic transaction completed

---
+++ src/AlphaRouterService.js
@@ -0,0 +1,93 @@
+const { AlphaRouter } = require("@uniswap/smart-order-router");
+const { UniswapV3Pool } = require("./constants/UniswapV3Pool.js");
+const {
+    Token,
+    CurrencyAmount,
+    TradeType,
+    Percent,
+} = require("@uniswap/sdk-core");
+const { ethers, BigNumber } = require("ethers");
+const JSBI = require("jsbi");
+const ERC20ABI = require("./abi.json");
+
+const V3_SWAP_ROUTER_ADDRESS = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
+const POLYGON_MUMBAI_RPC_URL = process.env.REACT_APP_POLYGON_MUMBAI_RPC_URL;
+
+const chainId = 80001;
+
+const web3Provider = new ethers.providers.JsonRpcProvider(
+    POLYGON_MUMBAI_RPC_URL
+);
+const router = new AlphaRouter({ chainId: chainId, provider: web3Provider });
+
+const name0 = "Neoverse Token";
+const symbol0 = "NEO";
+const decimals0 = 18;
+const address0 = "0x7007D4Dc65D768e275DDA842deB1cD793cf99642";
+
+const name1 = "Wrapped Matic";
+const symbol1 = "Wmatic";
+const decimals1 = 18;
+const address1 = "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889";
+const poolAddress = "0xE903643041cDB499750E2715B53254b92767142f";
+
+const NEO = new Token(chainId, address0, decimals0, symbol0, name0);
+const WMATIC = new Token(chainId, address1, decimals1, symbol1, name1);
+
+export const getNeoContract = () =>
+    new ethers.Contract(address0, ERC20ABI, web3Provider);
+export const getWmaticContract = () =>
+    new ethers.Contract(address1, ERC20ABI, web3Provider);
+export const getPoolContract = () =>
+    new ethers.Contract(poolAddress, UniswapV3Pool, web3Provider);
+
+export const getPrice = async(
+    inputAmount,
+    slippageAmount,
+    deadline,
+    walletAddress
+) => {
+    const percentSlippage = new Percent(slippageAmount, 100);
+    const neoDecimals = ethers.utils.parseUnits(
+        inputAmount.toString(),
+        decimals0
+    );
+    const currencyAmount = CurrencyAmount.fromRawAmount(
+        NEO,
+        JSBI.BigInt(neoDecimals)
+    );
+
+    const route = await router.route(
+        currencyAmount,
+        WMATIC,
+        TradeType.EXACT_INPUT, {
+            recipient: walletAddress,
+            slippageTolerance: percentSlippage,
+            deadline: deadline,
+        }
+    );
+
+    const transaction = {
+        data: route.methodParameters.calldata,
+        to: V3_SWAP_ROUTER_ADDRESS,
+        value: BigNumber.from(route.methodParameters.value),
+        from: walletAddress,
+        gasPrice: BigNumber.from(route.gasPriceWei),
+        gasLimit: ethers.utils.hexlify(1000000),
+    };
+
+    const quoteAmountOut = route.quote.toFixed(6);
+    const ratio = (inputAmount / quoteAmountOut).toFixed(3);
+
+    return [transaction, quoteAmountOut, ratio];
+};
+
+export const runSwap = async(transaction, signer) => {
+    const approvalAmount = ethers.utils.parseUnits("10", 18).toString();
+    const neoTokenContract = getNeoContract();
+    await neoTokenContract
+        .connect(signer)
+        .approve(V3_SWAP_ROUTER_ADDRESS, approvalAmount);
+
+    signer.sendTransaction(transaction);
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,239 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { GearFill } from "react-bootstrap-icons";
+
+import PageButton from "./components/PageButton";
+import ConnectButton from "./components/ConnectButton";
+import ConfigModal from "./components/ConfigModal";
+import CurrencyField from "./components/CurrencyField";
+
+import BeatLoader from "react-spinners/BeatLoader";
+import {
+  getNeoContract,
+  getWmaticContract,
+  getPrice,
+  runSwap,
+  getPoolContract,
+} from "./AlphaRouterService";
 
 function App() {
+  const [provider, setProvider] = useState(undefined);
+  const [signer, setSigner] = useState(undefined);
+  const [signerAddress, setSignerAddress] = useState(undefined);
+
+  const [slippageAmount, setSlippageAmount] = useState(2);
+  const [deadlineMinutes, setDeadlineMinutes] = useState(10);
+  const [showModal, setShowModal] = useState(undefined);
+
+  const [inputAmount, setInputAmount] = useState(undefined);
+  const [outputAmount, setOutputAmount] = useState(undefined);
+  const [transaction, setTransaction] = useState(undefined);
+  const [loading, setLoading] = useState(undefined);
+  const [ratio, setRatio] = useState(undefined);
+  const [poolcontract, setPoolcontract] = useState(undefined);
+  const [neoContract, setNeoContract] = useState(undefined);
+  const [wmaticContract, setWmaticContract] = useState(undefined);
+  const [neoAmount, setNeoAmount] = useState(undefined);
+  const [wmaticAmount, setWmaticAmount] = useState(undefined);
+  const [transactionDirection, setTransactionDirection] = useState(true);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider);
+
+      const poolContract = getPoolContract();
+      setPoolcontract(poolContract);
+
+      const neoContract = getNeoContract();
+      setNeoContract(neoContract);
+
+      const wmaticContract = getWmaticContract();
+      setWmaticContract(wmaticContract);
+    };
+    onLoad();
+  }, []);
+
+  const getSigner = async (provider) => {
+    provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    setSigner(signer);
+  };
+  const isConnected = () => signer !== undefined;
+  const getWalletAddress = () => {
+    signer.getAddress().then((address) => {
+      setSignerAddress(address);
+
+      // todo: connect neo and wmatic contracts
+      neoContract.balanceOf(address).then((res) => {
+        setNeoAmount(Number(ethers.utils.formatEther(res)));
+      });
+      wmaticContract.balanceOf(address).then((res) => {
+        setWmaticAmount(Number(ethers.utils.formatEther(res)));
+      });
+    });
+  };
+
+  if (signer !== undefined) {
+    getWalletAddress();
+  }
+
+  const getSwapPrice = (inputAmount) => {
+    setLoading(true);
+    setInputAmount(inputAmount);
+
+    const swap = getPrice(
+      inputAmount,
+      slippageAmount,
+      Math.floor(Date.now() / 1000 + deadlineMinutes * 60),
+      signerAddress
+    ).then((data) => {
+      setTransaction(data[0]);
+      setOutputAmount(data[1]);
+      setRatio(data[2]);
+      setLoading(false);
+    });
+  };
+
+  const setTxdirec = () => {
+    setTransactionDirection(!transactionDirection);
+    console.log(`Transaction Direction set to ${transactionDirection}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="appNav">
+        <div className="my-2 buttonContainer buttonContainerTop">
+          {transactionDirection ? (
+            <>
+              <PageButton name={"Neo to Matic"} isBold={"isBold"} />
+              <PageButton name={"Matic to Neo"} />{" "}
+            </>
+          ) : (
+            <>
+              <PageButton name={"Neo to Matic"} />
+              <PageButton name={"Matic to Neo"} isBold={"isBold"} />
+            </>
+          )}
+        </div>
+        <i className="fa-solid fa-retweet mx-2" onClick={setTxdirec} />
+
+        <div className="rightNav">
+          <div className="connectButtonContainer">
+            <ConnectButton
+              provider={provider}
+              isConnected={isConnected}
+              signerAddress={signerAddress}
+              getSigner={getSigner}
+            />
+          </div>
+          <div className="my-2 buttonContainer">
+            <PageButton name={"..."} isBold={true} />
+          </div>
+        </div>
+      </div>
+
+      <div className="appBody">
+        <div className="swapContainer">
+          <div className="swapHeader">
+            <span className="swapText">Swap</span>
+            <span className="gearContainer" onClick={() => setShowModal(true)}>
+              <GearFill />
+            </span>
+            {showModal && (
+              <ConfigModal
+                onClose={() => setShowModal(false)}
+                setDeadlineMinutes={setDeadlineMinutes}
+                deadlineMinutes={deadlineMinutes}
+                setSlippageAmount={setSlippageAmount}
+                slippageAmount={slippageAmount}
+              />
+            )}
+          </div>
+          {transactionDirection ? (
+            <>
+              <div className="swapBody">
+                <CurrencyField
+                  field="input"
+                  tokenName="NEO"
+                  getSwapPrice={getSwapPrice}
+                  signer={signer}
+                  balance={neoAmount}
+                />
+                <CurrencyField
+                  field="output"
+                  tokenName="WMATIC"
+                  value={outputAmount}
+                  signer={signer}
+                  balance={wmaticAmount}
+                  spinner={BeatLoader}
+                  loading={loading}
+                />
+              </div>
+
+              <div className="ratioContainer">
+                {ratio && <>{`1 WMATIC = ${ratio} NEO`}</>}
+              </div>
+            </>
+          ) : (
+            <>
+              {/* {" "}
+              <div className="swapBody">
+                <CurrencyField
+                  field="input"
+                  tokenName="WMATIC"
+                  getSwapPrice={getSwapPrice}
+                  signer={signer}
+                  balance={wmaticAmount}
+                />
+                <CurrencyField
+                  field="output"
+                  tokenName="NEO"
+                  value={1 / outputAmount}
+                  signer={signer}
+                  balance={neoAmount}
+                  spinner={BeatLoader}
+                  loading={loading}
+                />
+              </div>
+              <div className="ratioContainer">
+                {ratio && <>{`1 NEO = ${1 / ratio} WMATIC`}</>}
+              </div> */}
+            </>
+          )}
+
+          <div>
+            <div className="swapButtonContainer">
+              {isConnected() ? (
+                <div
+                  style={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <div
+                    style={{ width: "45%" }}
+                    onClick={() => runSwap(transaction, signer)}
+                    className="swapButton"
+                  >
+                    Neo to Matic
+                  </div>
+                  <div
+                    style={{ width: "45%" }}
+                    onClick={() => runSwap(transaction, signer)}
+                    className="swapButton"
+                  >
+                    Matic To Neo
+                  </div>
+                </div>
+              ) : (
+                <div onClick={() => getSigner(provider)} className="swapButton">
+                  Connect Wallet
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
